14674c93f8d6d363dc76cfdd1ea9d43d7c291b58
Mateus Henrique <mateushbsb@gmail.com>
2022-07-19 16:56:21 -0300

    feat[observer]: add find company name by observer

--- src/modules/observer/services/IndexObserverService.ts
+++ src/modules/observer/services/IndexObserverService.ts
@@ -1,12 +1,26 @@
 import { getCustomRepository } from 'typeorm';
 
 import ObserverRepository from '../repositories/ObserverRepository';
+import CompanyRepository from '@modules/company/repositories/CompanyRepository';
 
 class IndexObserverService {
   async execute() {
     const observerRepository = getCustomRepository(ObserverRepository);
+    const companyRepository = getCustomRepository(CompanyRepository);
 
-    return observerRepository.findAll();
+    const observers = await observerRepository.findAll();
+    const companies = await companyRepository.findAll();
+
+    const observerWithCompany = observers.map(observer => {
+      return { 
+        id: observer.id,
+        name: observer.name,
+        company_id: observer.company_id,
+        company_name: companies.find(company => company.id === observer.company_id).name
+      }
+    })
+
+    return observerWithCompany;
   }
 }
 
