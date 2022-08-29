18611c88f2b5bcf71304d51f30ecbb71a34723be
Mateus Henrique <mateushbsb@gmail.com>
2022-08-29 10:25:47 -0300

    feat[ros]: add rows with anonymous observer

--- src/modules/ros/services/CreateRosService.ts
+++ src/modules/ros/services/CreateRosService.ts
@@ -30,29 +30,52 @@ class CreateRosService {
     suggestion,
     isAvail
   }: UserRequest) {
-    console.log(date);
-
     const rosRepository = getCustomRepository(RosRepository);
 
-    const ros = rosRepository.create({
-      date: new Date(date),
-      observer_id: name,
-      local_id: local,
-      zone_id: zone,
-      nature_id: nature,
-      reason_id: reason,
-      company_area: companyArea,
-      description,
-      suggestion,
-      isAvail_responsible: Boolean(isAvail),
-      status: 'não resolvido',
-      negotiations: 'não tratado',
-      created_at: new Date(),
-    });
+    let ros = {};
 
-    await rosRepository.save(ros);
+    if (name === "") {
+      ros = rosRepository.create({
+        date: new Date(date),
+        local_id: local,
+        zone_id: zone,
+        nature_id: nature,
+        reason_id: reason,
+        company_area: companyArea,
+        description,
+        suggestion,
+        isAvail_responsible: Boolean(isAvail),
+        status: 'não resolvido',
+        negotiations: 'não tratado',
+        created_at: new Date(),
+      });
 
-    return ros;
+      await rosRepository.save(ros); 
+
+      return ros;
+    }
+
+    if(name) {
+      ros = rosRepository.create({
+        date: new Date(date),
+        observer_id: name,
+        local_id: local,
+        zone_id: zone,
+        nature_id: nature,
+        reason_id: reason,
+        company_area: companyArea,
+        description,
+        suggestion,
+        isAvail_responsible: Boolean(isAvail),
+        status: 'não resolvido',
+        negotiations: 'não tratado',
+        created_at: new Date(),
+      });
+  
+      await rosRepository.save(ros); 
+  
+      return ros;
+    }
   }
 
 }
