e394d47c48b872f7e8bbcb8b12f8251a05d35a1d
Mateus Henrique <mateushbsb@gmail.com>
2022-10-12 15:31:10 -0300

    feat[comments]: get ros by responsible controller

--- src/modules/ros/infra/http/routes/ros-responsible.router.ts
+++ src/modules/ros/infra/http/routes/ros-responsible.router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import RosResponsibleController  from '../controllers/RosResponsibleController';
+import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const rosResponsibleController = new RosResponsibleController();
 
 const router = Router();
 
-router.get('/:id', rosResponsibleController.index);
+router.get('/', ensureAuthenticated, rosResponsibleController.index);
 
 export default router;
